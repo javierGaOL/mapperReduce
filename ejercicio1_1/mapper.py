dbe3b9e3f626110e150094dfb92408b9b2ab3d70
javierGaOL <waver577@gmail.com>
2023-12-01 11:11:14 +0100

    exercicio1_1

--- ejercicio1_1/mapper.py
+++ ejercicio1_1/mapper.py
@@ -1,9 +1,8 @@
-
 #!/usr/bin/python
 import sys
 
 for line in sys.stdin:
-    if ("\t") in line:
+    if line.count("\t")==5:
         data = line.strip().split("\t")
         date, time, store, item, cost, payment = data
         print(store+"\t"+cost)
