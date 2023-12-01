3b4d5876cba3ac08355f5327724dbbee2283e5dd
javierGaOL <waver577@gmail.com>
2023-12-01 14:04:14 +0100

    ejercicio2

--- ejercicio1_2/mapper.py
+++ ejercicio1_2/mapper.py
@@ -6,7 +6,7 @@ for line in sys.stdin:
     if ("\t") in line:
         data = line.strip().split("\t")
         date, time, store, item, cost, payment = data
-        print(store+"\t"+cost)
+        print(item+"\t"+cost)
 
 
 
--- ejercicio1_2/reducer.py
+++ ejercicio1_2/reducer.py
@@ -3,27 +3,37 @@ import sys
 
 salesTotal = 0
 oldKey = None
-
+totalPorCategoria={}
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
+    #if len(data_mapped) != 2:
         # Something has gone wrong. Skip this line.
-        continue
+        #continue
 
     thisKey, thisSale = data_mapped
+    #store,item,cost,payment=data_mapped
 
     # Escribe un par key:value ante un cambio na key
     # Reinicia o total
-    if oldKey and oldKey != thisKey:
-        print(oldKey+"\t"+str(salesTotal))
-        oldKey = thisKey;
-        salesTotal = 0
+    #if oldKey and oldKey != thisKey:
+        #print(oldKey+"\t"+str(salesTotal))
+        #oldKey = thisKey;
+        #salesTotal = 0
+    if data_mapped[0] not in totalPorCategoria:
+         totalPorCategoria[data_mapped[0]]=data_mapped[1]
+    else:
+         totalPorCategoria[data_mapped[0]] =float(totalPorCategoria[data_mapped[0]]) + float(data_mapped[1])
 
-    oldKey = thisKey
-    salesTotal += float(thisSale)
+
+    #oldKey = thisKey
+    #salesTotal += float(thisSale)
 
 # Escribe o ultimo par, unha vez rematado o bucle
-if oldKey != None:
-    print(oldKey+"\t"+str(salesTotal))
+#if oldKey != None:
+ #   print(oldKey+"\t"+str(salesTotal))
+
+#Ficheiro de probas
 
 
+
+print(totalPorCategoria)
